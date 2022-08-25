bff4cf999262ad916b91bd86765d927ba84383a0
wsft <contact@winisreallybored.com>
2022-08-25 02:48:13 -0700

    Rbvolt initial commit

---
+++ lib/rbvolt/client.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "json"
+require "logger"
+
+require "async"
+require "async/websocket/client"
+require_relative "./utils/colored_puts"
+
+module Rbvolt
+  #
+  # Class for connecting to Revolt.
+  #
+  class Client
+  end
+
+end
---
+++ lib/rbvolt/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rbvolt
+  VERSION = "0.1.0"
+end
